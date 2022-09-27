95d424cff64f7f8e8a95abec613d07a235aa0dd3
NINAN VARGHESE <NINAN VARGHESE@NINANVARGHESE>
2022-09-27 12:43:57 +0530

    Listing key value pair of sample cde of HashMap

--- JavaCoding/src/JavaCollections/ColHashMap.java
+++ JavaCoding/src/JavaCollections/ColHashMap.java
@@ -1,6 +1,7 @@
 package JavaCollections;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class ColHashMap {
 	public static void main(String[] args) {
@@ -12,8 +13,16 @@ public class ColHashMap {
 		map.put(4, "Four");
 		map.put(2, "Two");
 		
+		//Replacing key value of "Eight" with "Hello" for key 8 
 		map.put(8, "Hello");
 		String text = map.get(8);
 		System.out.println(text);
+		
+		for(Map.Entry<Integer, String> entry:map.entrySet()) {
+			int key = entry.getKey();
+			String value = entry.getValue();
+			
+			System.out.println(key+" : "+value);
+		}
 	}
 }
