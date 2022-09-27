7b3f47ca4795fad4ccecb5b4ea8627e9222c7587
NINAN VARGHESE <NINAN VARGHESE@NINANVARGHESE>
2022-09-27 20:02:12 +0530

    Added a sample code for Sorted Map alongwith HashMap and Linked HashMap

---
+++ JavaCoding/src/JavaCollections/ColSortedMap.java
@@ -0,0 +1,39 @@
+package JavaCollections;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ColSortedMap {
+
+	public static void main(String[] args) {
+		Map<Integer, String> hashMap = new HashMap<Integer, String>();
+		Map<Integer, String> linkedHashMap = new LinkedHashMap<Integer, String>();
+		Map<Integer, String> treeMap = new TreeMap<Integer, String>();
+		
+		System.out.println("The list for HashMap(Unsorted/Random Map) as follows:");
+		testMap(hashMap);
+		System.out.println("\nThe list for LinkedHashMap(Map that follows the same order as mentioned) as follows: ");
+		testMap(linkedHashMap);
+		System.out.println("\nThe list for TreeMap(sorted Map) as follows: ");
+		testMap(treeMap);
+	}
+
+	private static void testMap(Map<Integer, String> map) {
+		map.put(9,  "Carmella");
+		map.put(7,  "Lisa");
+		map.put(12,  "Nicole");
+		map.put(8,  "Angela");
+		map.put(1,  "Amy");
+		map.put(0,  "Natalia");
+		map.put(15,  "Kiara");
+		map.put(6,  "Jessy");
+		
+		for(Integer key:map.keySet()) {
+			String value = map.get(key);
+			System.out.println(key+ " : " +value);
+		}
+	}
+
+}
